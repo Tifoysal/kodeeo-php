d0df0d862848196a498b558b1d031dd8f5db1c4c
sanjidaakter13 <sanjidaakter1311@gmail.com>
2022-08-24 10:45:22 -0700

    comparison & logical operator

---
+++ sanjida/comparison-operator.php
@@ -0,0 +1,7 @@
+<?php
+// comarison operator
+$c=24;
+$d=30;
+echo "for c==d, the result is " ;
+echo var_dump ($c==$d). "<br>";
+?>
---
+++ sanjida/logical-operator.php
@@ -0,0 +1,14 @@
+<?php
+//logical operator
+$m="true";
+$n="false";
+echo "for m and n, the result is ";
+echo var_dump ($m and $n);
+echo "<br>";
+echo "for m or n, the result is ";
+echo var_dump ($m or $n);
+echo "<br>";
+echo "for !m, the result is ";
+echo var_dump (!$m);
+echo "<br>";
+?>
