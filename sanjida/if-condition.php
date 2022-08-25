322deb42183332cdc19a538ec0fcbf665e008ac4
sanjidaakter13 <sanjidaakter1311@gmail.com>
2022-08-25 10:37:41 -0700

    If & switch statement

---
+++ sanjida/if-condition.php
@@ -0,0 +1,12 @@
+<?php
+//if else condition;
+$age=24;
+if ($age> 25){
+    echo "Faizan is a little boy. <br>";
+} 
+elseif($age> 65){
+    echo "He is an old man.<br>";
+}
+else{
+    echo " He can drive a car.<br>";
+}?>
